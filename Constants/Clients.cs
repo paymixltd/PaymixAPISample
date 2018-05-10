1ba96dc7e7d7fa36ee3ba3f3a1d3aeca71962dfc
Savas Manyasli <sm@paymix.euc>
2018-05-10 18:05:59 +0200

    Add project files.

---
+++ Constants/Clients.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constants
+{
+    public static class Clients
+    {
+        public readonly static Client Client1 = new Client
+        {
+            Id = "client2",
+            Secret = "client2",
+            RedirectUrl = Paths.AuthorizeCodeCallBackPath
+        };
+
+        public readonly static Client Client2 = new Client
+        {
+            Id = "client2",
+            Secret = "client2",
+            RedirectUrl = Paths.ImplicitGrantCallBackPath
+        };
+    }
+
+    public class Client
+    {
+        public string Id { get; set; }
+        public string Secret { get; set; }
+        public string RedirectUrl { get; set; }
+    }
+}
---
+++ Constants/Paths.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constants
+{
+    public static class Paths
+    {
+        /// <summary>
+        /// AuthorizationServer project should run on this URL
+        /// </summary>
+        public const string AuthorizationServerBaseAddress = "http://neropaytest.paymix.eu:8182";
+
+        /// <summary>
+        /// ResourceServer project should run on this URL
+        /// </summary>
+        public const string ResourceServerBaseAddress = "http://neropaytest.paymix.eu:8182";
+
+        /// <summary>
+        /// ImplicitGrant project should be running on this specific port '38515'
+        /// </summary>
+        public const string ImplicitGrantCallBackPath = "http://localhost:38515/Home/SignIn";
+
+        /// <summary>
+        /// AuthorizationCodeGrant project should be running on this URL.
+        /// </summary>
+        public const string AuthorizeCodeCallBackPath = "http://localhost:38500/";
+
+        public const string AuthorizePath = "/PaymixWS_Auth/oauth/authorize";
+        public const string TokenPath = "/PaymixWS_Auth/oauth/token";
+        
+        //public const string LoginPath = "/Account/Authorize";
+        //public const string LogoutPath = "/Account/Logout";
+        public const string MePath = "/PaymixWS_Resource/Members/Profile";
+    }
+}
